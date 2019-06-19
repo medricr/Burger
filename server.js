356372ffac5538252ce263f0f4a354054b0648f6
Medric Riley <medricr@gmail.com>
2019-06-18 21:28:46 -0700

    established connection with database form server.js

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 // allow app to serve static files
 app.use(express.static('public'));
+// set up handlebars functionality
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// require mysql
+var mysql = require("mysql");
+// set up connection to mysql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "movies_db"
+});
+// open connection to database
+connection.connect(function(err){
+    if(err) throw err;
+    console.log('connected as id ' + connection.threadId);
+})
 // start server listening
 app.listen(PORT,function(){
     console.log('app listening on port ' + PORT);
