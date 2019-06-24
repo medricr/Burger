d92dfbf931ac495a66ddfac3ff810f9e3c2862ff
Medric Riley <medricr@gmail.com>
2019-06-24 16:27:50 -0700

    finished functionality for viewing, adding, and changing the status of burgers

--- models/burger.js
+++ models/burger.js
@@ -5,7 +5,20 @@ var burger = {
         orm.selectAll('burger_table',function(res){
             cb(res);
         })
+    },
+    create: function(cols, vals, cb){
+        console.log('create fire');
+        orm.insertOne('burger_table',cols,vals,function(res){
+            cb(res);
+        })
+    },
+    update: function(objColVals, condition, cb){
+        orm.updateOne('burger_table',objColVals, condition, function(res){
+            console.log(res);
+            cb(res);
+        });
     }
+
 }
 
 module.exports = burger;
