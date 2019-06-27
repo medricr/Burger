95166eaf8f9565050809cbfbed8b11e363bfacbc
Medric Riley <medricr@gmail.com>
2019-06-26 20:22:08 -0700

    connected to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,24 @@
 // require mysql
 var mysql = require("mysql");
 // set up connection to mysql database
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+if(process.env.JAWSDB_URL){
+	var connection = mysql.createConnection(JAWSDB_URL);
+}
+else {
+	var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "password",
+	database: "burgers_db"
 });
+}
+
 // open connection to database
 connection.connect(function(err){
     if(err) throw err;
     console.log('connected as id ' + connection.threadId);
+	
 })
 // export connection for use elsewhere in the program
 module.exports = connection;
